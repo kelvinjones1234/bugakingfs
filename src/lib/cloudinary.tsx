470f134e45a9ced9e5c5a14b2dc4c631cc773f43
kelvinjones1234 <praisejournal2@gmail.com>
2026-02-23 14:55:44 +0100

    added stuff

--- src/lib/cloudinary.tsx
+++ src/lib/cloudinary.tsx
@@ -5,7 +5,7 @@ cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
-});
+}); 
 
 export const uploadImage = async (file: File, folder: string) => {
   const arrayBuffer = await file.arrayBuffer();
